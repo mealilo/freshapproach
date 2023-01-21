4485b559f0d7b799f78fa7abf5e03e638ccf6659
Jenna <jennaweller7@gmail.com>
2023-01-20 21:24:12 -0800

    newchanges

---
+++ components/ProductData.js
@@ -0,0 +1,30 @@
+import fruitPic from "../public/images/Fruit.png";
+import veggiesPic from "../public/images/Veggies.png";
+import nutsPic from "../public/images/Nuts.png";
+import honeyPic from "../public/images/Honey.png";
+import eggsPic from "../public/images/Eggs.png";
+
+
+export const productData = [
+{
+    key:1,
+    img: fruitPic,
+    description: "Fruit",
+},
+{   key:2,
+    img: veggiesPic,
+    description: "Veggies",
+},
+{   key:3,
+    img:nutsPic,
+    description: "Nuts",
+},
+{   key:4,
+    img:honeyPic,
+    description: "Honey",
+},
+{   key:5,
+    img:eggsPic,
+    description: "Eggs",
+}
+]
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Banner from "../components/Banner"
+import SmallCard from "../components/SmallCard"
+import { productData } from '../components/ProductData'; 
 export default function Home() {
   return (
     <div className="">
@@ -11,11 +13,22 @@ export default function Home() {
     
     <Header/>
     <Banner/>
-    <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+    <main className="max-w-7xl mx-auto px-8 sm:px-16">
       <section className="pt-6">
-        <h2 className="text-4xl font-semibold pb-5"> Explore Produce </h2>
+        <h2 className="text-4xl font-semibold pb-5 text-center text-cyan-800"> Explore Produce </h2>
+          {/* pull some data from a server */}
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {productData.map(({key, img, description}) => (
+        <SmallCard 
+        key={key} 
+        img={img}
+        description={description} />
+        ))}
+        </div>
       </section>
     </main>
     </div>
   );
 }
+
+
