a5327e3ddeede32cf4e866fcc88d2cdfada7a2f3
Jenna <jennaweller7@gmail.com>
2023-01-23 15:36:33 -0800

    linkingtootherpages

---
+++ pages/ExploreProduce.js
@@ -0,0 +1,30 @@
+
+import Image from "next/image";
+import Link from 'next/link';
+import Head from 'next/head'
+import Header from '../components/Header'
+import Footer from "../components/Footer"
+export default function Home() {
+  return (
+    <div className="">
+     <Head>
+        <title> Close Crop</title>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+    
+    <Header/>
+    <main className="max-w-7xl mx-auto px-8 sm:px-16">
+      <section className="pt-6">
+        <h2 className="text-4xl font-semibold pb-5 text-center text-greenText"> Discover Local Produce</h2>
+          {/* pull some data from a server */}
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
+     </div>
+      </section>
+   
+    </main>
+    <Footer />
+    </div>
+  );
+}
+
+
