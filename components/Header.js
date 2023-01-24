b6579ff415aa171b4dc0f515a575506180a81646
librow <55640527+librow@users.noreply.github.com>
2023-01-23 18:42:43 -0700

    misc fixes

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 
-import Link from "next/Link"
+import Link from "next/link"
 import Image from "next/image";
 import {MenuIcon, UserCircleIcon} from '@heroicons/react/solid';
 function Header() {
--- components/SmallCard.js
+++ components/SmallCard.js
@@ -1,10 +1,11 @@
+import Image from "next/image";
 
 function SmallCard({img, description}){
     return(
         <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition
         transform duration-200 ease-out">
         <div className="relative h-16 w-16">
-        <img src={img} />
+        <Image alt="fruit" src={img} />
         </div>
         <div>
             <h2>{description}</h2>
