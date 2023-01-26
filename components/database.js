ce7caa066c5a62f76d07f7d6544833b7b7b2ca30
Brennan Williams <brennanwilliams2000@gmail.com>
2023-01-25 21:15:44 -0700

    documentation

--- components/database.js
+++ components/database.js
@@ -4,25 +4,69 @@ const prisma = new PrismaClient()
 
 async function main() {
     // ... you will write your Prisma Client queries here
-    const Test = await prisma.Test.findMany()
-    console.log(Test)
-    // const allUsers = await prisma.user.findMany()
-    // console.log(allUsers)
-    // const allPosts = await prisma.profile.findMany();
-    // console.log(allPosts);
 
-    // // create user
-    await prisma.Test.create({
-        data: {
-            FIRST_NAME: "Hello world",
-            EMAIL: "test usDDDer2gmail.com"
+    // query for all people
+    const people = await prisma.person.findMany();
+    console.log(people);
 
-        }
-
+    //specific person
+    const user = await prisma.person.findUnique({
+        where: {
+            person_ID: 1,
+        },
     })
+    console.log(user)
 
-    const Test2 = await prisma.Test.findMany()
-    console.log(Test2)
+
+    //create user
+
+    //const date = new Date();
+    //console.log(date);
+    //  create user
+    //await prisma.person.create({
+    //    data: {
+    //        first_name: "Hello world",
+    //        last_name: "programming",
+    //        email: "testssDDDder2gmail.cddom",
+    //        password: "test usDDDer2gmail.com",
+    //        profile_picture_link: "test",
+    //        created_on: date
+    //    }
+
+    //})
+
+    //delete user
+
+    //const deleteUser = await prisma.person.delete({
+    //    where: {
+    //        person_ID: 3,
+    //    },
+    //})
+
+
+    // By ID
+
+
+    //update a specific user
+    const updateUser = await prisma.person.update({
+        where: {
+            person_ID: 1,
+        },
+        data: {
+            first_name: 'Viola the Magnificent',
+        },
+    })
+    //specific person
+    const updatedUSER = await prisma.person.findUnique({
+        where: {
+            person_ID: 1,
+        },
+    })
+    console.log(updatedUSER)
+    
+
+
+
 }
 
 main()
