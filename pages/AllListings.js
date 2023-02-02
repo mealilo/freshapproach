88f7146d0ba271da3bf9a45fa0267400541c563f
Brennan Williams <brennanwilliams2000@gmail.com>
2023-02-01 19:44:05 -0700

    working

--- pages/AllListings.js
+++ pages/AllListings.js
@@ -6,6 +6,8 @@ import prisma from '../lib/prisma';
 
 //all listings is generated below, it's the props or something I think? '
 const allListings = props => {
+
+ 
     return (
         <div className="page">
             <main>
@@ -13,8 +15,22 @@ const allListings = props => {
                     {props.items.map(listing => (
                         <div key={listing.listing_ID}>
                             <Listing listing={listing} />
+
+                            {/*{*/}
+                            {/*    listing.listing_picture.length > 0*/}
+                            {/*        ? {*/}
+                            {/*            listinglisting_picture.map(pictures => (*/}
+                            {/*            pictures.picture_link*/}
+                            {/*            ))}*/}
+                            {/*    : <h1>No Picture is available</h1>*/}
+                            {/*}*/}
+
+                            {
+                                listing.listing_picture.length > 0
+                                ? <img src={listing.listing_picture[0].picture_link} /> 
+                                : <h1>No Picture is available</h1>
+                            }
                             
-                            <img  src={ listing.listing_picture[0].picture_link }/>
                         </div>
 
                     ))}
@@ -29,19 +45,11 @@ export const getServerSideProps = async () => {
     const items = await prisma.listing.findMany({
         //include foregin keys, in this case there is a FK to listing_picture, so we make sure to include it
         include: {
-            listing_picture: {
-                select: {
-                    picture_link: true,
-                }
-            }
+            listing_picture: true,
         },
-        //include everything in foregin table
-        //include: {
-        //    listing_picture: true,
-        //},
 
     })
-    console.log("Picture LINK")
+    console.log("Picture LINK:")
     console.log(items[0].listing_picture[0].picture_link);
     //console.log(items);
     return {
