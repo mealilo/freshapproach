de08975b8b1e942c6e93b3e31e6a8d0e9788a4b2
librow <55640527+librow@users.noreply.github.com>
2023-02-15 20:25:36 -0700

    fixed vendor signup page styling

--- pages/BecomeVendor.js
+++ pages/BecomeVendor.js
@@ -45,155 +45,116 @@ const submitHandler = (event) => {
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6"></section>
         <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-          <div className="w-full max-w-3xl space-y-8">
+          <div className= "w-full max-w-3xl space-y-8">
             <div className="space-y-5 rounded-md shadow-xl p-5 columns-2 border-t-8 border-teal-800">
-              <h2 className=" text-left text-5xl font-normal tracking-tight text-gray-900">
+              <h2 className="text-left text-5xl font-normal tracking-tight text-gray-900">
                 Vendor Sign Up
               </h2>
             </div>
             <form onSubmit={submitHandler} type="POST" className="mt-8 space-y-6">
               <input type="hidden" name="remember" defaultValue="true" />
-              <div className="space-y-5 rounded-md shadow-xl p-8 columns-2 border-t-8 border-orange-400">
-                <div className="flex flex-col space-y-2">
-                  <label htmlFor="fname">First Name</label>
-                  <input
-                    id="fname"
-                    name="fname"
-                    type="text"
-                    autoComplete="fname"
-                    required
-                    className="relative block w-full appearance-none rounded-none rounded-t-md rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    placeholder="Enter your First Name"
-
-                  />
-                </div>
-                <div className="flex flex-col space-y-2">
-                  <label htmlFor="lname">Last Name</label>
-                  <input
-                    id="lname"
-                    name="lname"
-                    type="text"
-                    autoComplete="lname"
-                    required
-                    className="relative block w-full appearance-none rounded-none rounded-t-md rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    placeholder="Enter your Last Name"
- 
-                  />
-                </div>
-                <div className="flex flex-col justify-end space-y-2">
+              <div className="rounded-md space-y-2 shadow-xl p-8 border-t-8 border-orange-400">
+                <h4 className="text-xl font-medium">Account Information</h4>
+                <div className="columns-2">
                   <div>
-
-                    
-                    <label htmlFor="email">Email</label>
-                    <input
-                      id="email"
-                      name="email"
-                      type="email"
-                      required
-                      className="relative block w-full appearance-none rounded-none rounded-b-md rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm font"
-                      placeholder="Enter your Email"
+                    <div>
+                      <label htmlFor="fname">First Name</label>
+                      <input
+                        id="fname"
+                        name="fname"
+                        type="text"
+                        autoComplete="fname"
+                        required
+                        className="relative block w-full appearance-none rounded-none rounded-t-md rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        placeholder="Enter your First Name"
                       />
-                     <label
-                      htmlFor="publicEmail"
-                      className="ml-2 block text-sm text-gray-900">
-                      Email Available to Public
-                    </label>
+                      <div className="pt-5"></div>
+                    </div>
+                    <div>
+                      <label htmlFor="email">Email</label>
+                      <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        required
+                        className="relative block w-full appearance-none rounded-none rounded-b-md rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm font"
+                        placeholder="Enter your Email"
+                      />
+                    </div>
+                  </div>
+                  
+                  <div className="flex flex-col">
+                    <label htmlFor="lname">Last Name</label>
                     <input
-                      id="publicEmail"
-                      name="publicEmail"
-                      type="checkbox"
-                      
-                      className="h-4 w-4 rounded border-gray-300 text-lime-600 focus:ring-lime-500"
+                      id="lname"
+                      name="lname"
+                      type="text"
+                      autoComplete="lname"
+                      required
+                      className="relative block w-full appearance-none rounded-none rounded-t-md rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                      placeholder="Enter your Last Name"
+  
                     />
-
+                    <div className="pt-5" />
+                  </div>
+                  <div className="flex flex-col">
+                    <label htmlFor="password">Password</label>
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="relative block w-full appearance-none rounded-none rounded-b-md rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                      placeholder="Create your Password"
+                    />
+                    <div className="pt-5" />
                   </div>
                 </div>
-
-                <div className="flex flex-col space-y-2">
-                  <label htmlFor="password">Password</label>
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    className="relative block w-full appearance-none rounded-none rounded-b-md rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    placeholder="Create your Password"
-                                      
-                  />
-                </div>
-                <div className="flex flex-col space-y-2">
-                  <label htmlFor="city">City</label>
-                  <input
-                    id="city"
-                    name="city"
-                    type="text"
-                    autoComplete="city"
-                    required
-                    className="relative block w-full appearance-none rounded-none rounded-t-md rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    placeholder="Enter your City"
-                            
-                  />
-                  
-
-                  <label
-                      htmlFor="publicAddress"
-                      className="ml-2 block text-sm text-gray-900">
-                      Make Address Available to Public
-                    </label>
+                <h4 className="text-xl font-medium">Public Details</h4>
+                <div className="columns-2">
+                  <div className="flex flex-col ">
+                    <label htmlFor="zip">Phone Number</label>
                     <input
-                      id="publicAddress"
-                      name="publicAddress"
-                      type="checkbox"
-                      
-                      className="h-4 w-4 rounded border-gray-300 text-lime-600 focus:ring-lime-500"
+                      id="phone"
+                      name="phone"
+                      type="phone"
+                      autoComplete="phone"
+                      required
+                      className="relative block w-full appearance-none rounded-none rounded-t-md rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                      placeholder="Enter your Phone Number"
                     />
-
-                  
-                </div>
-                <div className="flex flex-col space-y-2">
-                  <label htmlFor="zip">Zip Code</label>
-                  <input
-                    id="zip"
-                    name="zip"
-                    type="text"
-                    autoComplete="zip"
-                    required
-                    className="relative block w-full appearance-none rounded-none rounded-t-md rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    placeholder="Enter your Zip Code"
-                  />
-                </div>
-
-                <div className="flex flex-col space-y-2">
-                  <label htmlFor="zip">Phone Number</label>
-                  <input
-                    id="phone"
-                    name="phone"
-                    type="phone"
-                    autoComplete="phone"
-                    required
-                    className="relative block w-full appearance-none rounded-none rounded-t-md rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    placeholder="Enter your Phone Number"
-                  />
-
-                    <label
-                      htmlFor="publicPhone"
-                      className="ml-2 block text-sm text-gray-900">
-                      Make Phone Available to Public
-                    </label>
-                    <input
-                      id="publicPhone"
-                      name="publicPhone"
-                      type="checkbox"
-                      
-                      className="h-4 w-4 rounded border-gray-300 text-lime-600 focus:ring-lime-500"
-                    />
+                  </div>
+                  <div className="columns-2">
+                    <div className="flex flex-col ">
+                      <label htmlFor="city">City</label>
+                      <input
+                        id="city"
+                        name="city"
+                        type="text"
+                        autoComplete="city"
+                        required
+                        className="relative block w-full appearance-none rounded-none rounded-t-md rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        placeholder="Enter your City"
+                                
+                      />
+                    </div>
+                    <div className="flex flex-col">
+                      <label htmlFor="zip">Zip Code</label>
+                      <input
+                        id="zip"
+                        name="zip"
+                        type="text"
+                        autoComplete="zip"
+                        required
+                        className="relative block w-full appearance-none rounded-none rounded-t-md rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        placeholder="Enter your Zip Code"
+                      />
+                      <div className="pt-5"></div>
+                    </div>
+                  </div>
                 </div>
               </div>
-
-
-              
-
               <div className="flex items-center justify-between">
                 <div className="flex items-center">
                   <input
