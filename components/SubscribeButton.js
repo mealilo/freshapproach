6601aaa6163d212bb0ebe692497531c32fa27186
Jenna Weller <65751108+JennaWeller@users.noreply.github.com>
2023-02-22 17:56:56 -0800

    Update SubscribeButton.js

--- components/SubscribeButton.js
+++ components/SubscribeButton.js
@@ -4,10 +4,11 @@ function SubscribeButton() {
       type="button"
       data-mdb-ripple="true"
       data-mdb-ripple-color="light"
-      className="font-Poppins inline-block px-6 py-2.5 bg-Sage text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-cyan-900 hover:shadow-lg focus:bg-cyan-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-cyan-800 active:shadow-lg transition duration-150 ease-in-out h6"
+      className="inline-block px-6 py-2.5 bg-Sage text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-cyan-900 hover:shadow-lg focus:bg-cyan-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-cyan-800 active:shadow-lg transition duration-150 ease-in-out h6"
     >
       Subscribe
     </button>
   );
 }
 export default SubscribeButton;
+
