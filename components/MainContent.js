93d719df7c579552d50d8ae7bc55dea75e871065
Jenna Weller <65751108+JennaWeller@users.noreply.github.com>
2023-02-26 16:29:40 -0800

    Update MainContent.js

--- components/MainContent.js
+++ components/MainContent.js
@@ -1,50 +1,78 @@
-
-import Image from "next/image";
-import Link from 'next/link';
-function MainContent(){
-    return(
-        <main className="max-w-7xl mx-auto px-8 sm:px-16 font-Poppins">
-        <section className="pt-6">
-          <h2 className="text-4xl font-semibold pb-5 text-center text-nearBlack font-Poppins"> Explore Produce </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
-      <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer " >
-           <img alt="fruit" src="/images/Fruit.png" className="w-36"/>
-           </div>
-           <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer">
-           <img alt="veggies" src="/images/Veggies.png" className="w-36"/>
-           </div>
-           <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer">
-           <img alt="nuts" src="/images/Nuts.png" className="w-36"/>
-           </div>
-           <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer">
-           <img alt="honey" src="/images/Honey.png" className="w-36"/>
-           </div>
-           <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer">
-           <img alt="eggs" src="/images/Eggs.png" className="w-36"/>
+import Link from "next/link";
+function MainContent() {
+  return (
+    <main className="max-w-7xl mx-auto px-8 sm:px-16 font-Poppins">
+      <section className="pt-6">
+        <h2 className="text-4xl font-semibold pb-5 text-center text-nearBlack font-Poppins">
+          {" "}
+          Explore Produce{" "}
+        </h2>
+        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-5 xl:grid-cols-7 space-x-10">
+          <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer "></div>
+          <div class="flex justify-center">
+            <div class="rounded-lg max-w-sm">
+              <a data-mdb-ripple="true" data-mdb-ripple-color="none">
+                <img class="rounded-t-lg w-18 md:w-32 lg:w-48 xl:w-52" src="/images/Fruit.png" alt="" />
+              </a>
+              <div class="p-6">
+                <h5 class="text-gray-900 text-xl text-center mb-2">Fruit</h5>
+              </div>
+            </div>
+          </div>
+          <div class="flex justify-center">
+            <div class="rounded-lg max-w-sm">
+              <a data-mdb-ripple="true" data-mdb-ripple-color="light">
+                <img class="rounded-t-lg w-18 md:w-32 lg:w-48 xl:w-52" src="/images/Veggies.png" alt="" />
+              </a>
+              <div class="p-6">
+                <h5 class="text-gray-900 text-xl text-center mb-2">Veggies</h5>
+              </div>
+            </div>
+          </div>
+          <div class="flex justify-center">
+            <div class="rounded-lg max-w-sm">
+              <a data-mdb-ripple="true" data-mdb-ripple-color="light">
+                <img class="rounded-t-lg w-18 md:w-32 lg:w-48 xl:w-52" src="/images/Nuts.png" alt="" />
+              </a>
+              <div class="p-6">
+                <h5 class="text-gray-900 text-xl text-center mb-2">Nuts</h5>
+              </div>
+            </div>
+          </div>
+          <div class="flex justify-center">
+            <div class="rounded-lg max-w-sm">
+              <a data-mdb-ripple="true" data-mdb-ripple-color="light">
+                <img class="rounded-t-lg w-18 md:w-32 lg:w-48 xl:w-52" src="/images/Honey.png" alt="" />
+              </a>
+              <div class="p-6">
+                <h5 class="text-gray-900 text-xl text-center mb-2">Honey</h5>
+              </div>
+            </div>
+          </div>
+          <div class="flex justify-center">
+            <div class="rounded-lg max-w-sm">
+              <a data-mdb-ripple="true" data-mdb-ripple-color="light">
+                <img class="rounded-t-lg w-18 md:w-32 lg:w-48 xl:w-52" src="/images/Eggs.png" alt="" />
+              </a>
+              <div class="p-6">
+                <h5 class="text-gray-900 text-xl text-center mb-2">Eggs</h5>
+              </div>
+            </div>
+          </div>
         </div>
-          </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5"> 
-          <p className="w-40 text-center"> Fruit</p>
-          <p className="w-40 text-center"> Veggies</p>
-          <p className="w-40 text-center"> Nuts</p>
-          <p className="w-40 text-center"> Honey</p>
-          <p className="w-40 text-center"> Eggs</p>
-          </div>
-          <Link href="/listings">
+
+        <Link href="/listings">
           <div class="flex justify-center items-center py-5 ">
-           <button type="button" class="inline-block px-6 py-2.5 bg-Orange text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-500 hover:shadow-lg focus:bg-orange-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-700 active:shadow-lg transition duration-150 ease-in-out">Explore Produce</button>
-         </div>
-         </Link>
-    
-    
-    
-        </section>
-     
-      </main>
-    );
+            <button
+              type="button"
+              class="inline-block px-6 py-2.5 bg-Orange text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-500 hover:shadow-lg focus:bg-orange-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-700 active:shadow-lg transition duration-150 ease-in-out"
+            >
+              Explore Produce
+            </button>
+          </div>
+        </Link>
+      </section>
+    </main>
+  );
 }
-export default MainContent
-
-
-
- 
+export default MainContent;
