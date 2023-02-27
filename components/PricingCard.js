26b6ebaf83cfa46a3daacadecee0c7315a07d582
Jenna Weller <65751108+JennaWeller@users.noreply.github.com>
2023-02-26 21:10:25 -0800

    Update PricingCard.js

--- components/PricingCard.js
+++ components/PricingCard.js
@@ -1,9 +1,9 @@
 function PricingCard (){
     return(
-        <div className="max-w-sm rounded overflow-hidden shadow-lg mx-auto my-8">
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">$35 per bushel</div>
-          <p className="text-gray-600 text-base">
+        <div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto my-8">
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">$35 per bushel</div>
+          <p class="text-gray-600 text-base">
             OPEN HOURS: M-Sat, 10-5 PM
           </p>
           <p className="text-gray-600 text-base">
@@ -13,7 +13,7 @@ function PricingCard (){
           type="button"
           data-mdb-ripple="true"
           data-mdb-ripple-color="light"
-          className="inline-block px-6 py-2.5 bg-cyan-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-cyan-900 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out h6">Contact Seller</button>
+          class="inline-block px-6 py-2.5 bg-cyan-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-cyan-900 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out h6">Contact Seller</button>
         </div>
       
         </div>
