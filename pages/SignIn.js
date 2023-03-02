21d8a9d15a15228449d602866ca0f53b5c62eef2
avbaker1998 <70988383+avbaker1998@users.noreply.github.com>
2023-03-02 15:01:49 -0700

    Fixed password holder

--- pages/SignIn.js
+++ pages/SignIn.js
@@ -78,7 +78,7 @@ export default function Home() {
                     autoComplete="current-password"
                     required
                     className="relative block w-full appearance-none rounded-none rounded-b-md rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    placeholder="Create your password"
+                    placeholder="Enter your password"
                   />
                 </div>
               </div>
