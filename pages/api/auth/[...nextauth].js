15d51da0118e1052118238ace8ae04c3d5471e99
Jenna <jennaweller7@gmail.com>
2023-03-02 13:54:17 -0800

    nextauthprisma

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+
+  //Override the default sign in page
+  secret: process.env.NEXTAUTH_SECRET,
+
+  page:{
+    signIn:"/auth/signin",
+  }
+}
+
+export default NextAuth(authOptions)
