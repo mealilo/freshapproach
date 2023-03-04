d5f7b7301f065b82fb5807fd3aedcbc8b180ce0b
librow <55640527+librow@users.noreply.github.com>
2023-03-04 15:40:13 -0700

    removing search bar

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import { MenuIcon, UserCircleIcon, SearchIcon } from "@heroicons/react/solid";
 import SubscribeButton from "../components/SubscribeButton";
 function Header() {
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 bg-nearBlack  p-5 md:px-60">
+    <header className="sticky top-0 z-50 grid grid-cols-2 bg-nearBlack  p-5 md:px-60">
       {/* Left */}
 
       <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -24,15 +24,6 @@ function Header() {
           <p className="text-white text-4xl font-Sahitya"> Close Crop </p>
         </div>
       </Link>
-      {/* Middle */}
-      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm w-64 bg-white">
-        <input
-          className="flex-grow pl-5 bg-white outline-none text-sm text-gray-600 placeholder-gray-400"
-          type="text"
-          placeholder="Search for Produce"
-        />
-        <SearchIcon className="hidden md:inline-flex h-8 bg-Sage text-white rounded-full p-2 cursor-pointer md:mx-2 " />
-      </div>
       {/* Right */}
       <div className="flex items-center space-x-10 justify-end">
         <Link href="/BecomeVendor">
