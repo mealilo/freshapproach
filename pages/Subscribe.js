9314870607d092ffe52e1f1a9b5768267aeeb346
Jenna Weller <65751108+JennaWeller@users.noreply.github.com>
2023-03-03 16:13:54 -0800

    Update Subscribe.js

--- pages/Subscribe.js
+++ pages/Subscribe.js
@@ -1,12 +1,11 @@
 import Head from "next/head";
 import SubscribeForm from "../components/SubscribeForm";
-import Link from "next/link";
 export default function Home() {
   return (
     <div className="">
       <Head>
         <title> Close Crop</title>
-        <Link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-16">
