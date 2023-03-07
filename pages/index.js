11140566b24e6a865e834ac51cd8fbcaa6e88cd4
Brennan Williams <brennanwilliams2000@gmail.com>
2023-03-06 21:45:50 -0700

    uncomment

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,8 @@ import Banner from "../components/Banner";
 import FindingLocalSection from "../components/FindingLocalSection";
 import MainContent from "../components/MainContent";
 import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
 export default function Home() {
-  const { data: session } = useSession();
+
   return (
     <div className="">
       <Head>
