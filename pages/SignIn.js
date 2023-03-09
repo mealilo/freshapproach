b0aae2ae2125b735fce644f29bcb7cf655f181ae
Jenna <jennaweller7@gmail.com>
2023-03-09 12:49:27 -0800

    changingSignin

--- pages/SignIn.js
+++ pages/SignIn.js
@@ -4,38 +4,44 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import sellerAccount from "./sellerAccount";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
-import SubscribeButton from "../components/SubscribeButton";
-
+import {useRouter} from "next/router";
 export default function Home() {
+  const router = useRouter(); 
   const { data: session } = useSession();
-
+ const handleSignIn = () => {
+  signIn("credentials", {redirect: false}).then((response) => {
+    if (response.ok) {
+      router.push("/sellerAccount");
+    }
+  });
+ };
   // Your submit handler function in JavaScript
-  const submitHandler = (event) => {
-    event.preventDefault();
+  // const submitHandler = (event) => {
+  //   event.preventDefault();
 
-    const formData = new FormData(event.target);
-    const data = Object.fromEntries(formData);
+  //   const formData = new FormData(event.target);
+  //   const data = Object.fromEntries(formData);
 
-    fetch("/api/authenticateUser", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-    .then((res) => res.json())
-    .then((response) => {
-      if (response.message === "Success") {
-        //redirect to wherever.
-        window.location = "/sellerAccount";
-      } else {
-        alert("Error: Please fill out form again");
-      }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-  };
+  //   fetch("/api/CreatePersonAndProducer", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(data),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((response) => {
+  //       if (response.message === "Success") {
+  //         //redirect to wherever.
+  //         window.location = "/";
+  //       } else {
+  //         alert("Error: Please fill out form again");
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error:", error);
+  //     });
+  // };
 
   return (
     <div className="">
@@ -52,77 +58,86 @@ export default function Home() {
                 Vendor Sign In
               </h2>
             </div>
-            <form
+            {/* <form
               onSubmit={submitHandler}
               type="POST"
               className="mt-8 space-y-6"
-            >
-              <input type="hidden" name="remember" defaultValue="true" />
-              <div className="space-y-5 rounded-md shadow-xl p-8 columns-2 border-t-8 border-orange-400">
-                <div className="flex flex-col justify-end">
-                  <div>
-                    <label htmlFor="email">Email</label>
-                    <input
-                      id="email"
-                      name="email"
-                      type="email"
-                      required
-                      className="relative block w-full appearance-none rounded-none rounded-b-md rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm font"
-                      placeholder="Enter your email"
-                    />
-                  </div>
-                </div>
-
-                <div className="flex flex-col">
-                  <label htmlFor="password">Password</label>
+            > */}
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="space-y-5 rounded-md shadow-xl p-8 columns-2 border-t-8 border-orange-400">
+              <div className="flex flex-col justify-end">
+                <div>
+                  <label htmlFor="email">Email</label>
                   <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
+                    id="email"
+                    name="email"
+                    type="email"
                     required
-                    className="relative block w-full appearance-none rounded-none rounded-b-md rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    placeholder="Enter your password"
+                    className="relative block w-full appearance-none rounded-none rounded-b-md rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm font"
+                    placeholder="Enter your email"
                   />
                 </div>
               </div>
 
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input
-                    id="remember-me"
-                    name="remember-me"
-                    type="checkbox"
-                    required
-                    className="h-4 w-4 rounded border-gray-300 text-lime-600 focus:ring-lime-500"
-                  />
-                  <label
-                    htmlFor="remember-me"
-                    className="ml-2 block text-sm text-gray-900"
-                  >
-                    Keep me signed in
-                  </label>
-                </div>
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-medium text-teal-800 hover:text-teal-600"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
+              <div className="flex flex-col">
+                <label htmlFor="password">Password</label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-b-md rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Enter your password"
+                />
               </div>
-              <div className="columns-2  ">
-                <div>
-                  <SubscribeButton white text="Cancel" type="submit" onClick={() => { window.location = "/" }} style="group relative flex w-full justify-center !text-black" />
-                </div>
-                <div>
-                <Link href="/sellerAccount">
-                  <SubscribeButton green text="Sign In" type="submit" onClick={signIn} style="group relative flex w-full justify-center" />
-                  </Link>
-                </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  required
+                  className="h-4 w-4 rounded border-gray-300 text-lime-600 focus:ring-lime-500"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Keep me signed in
+                </label>
               </div>
-            </form>
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-medium text-teal-800 hover:text-teal-600"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+            <div className="columns-2  ">
+              <div>
+                <button
+                  type="submit"
+                  className="group relative flex w-full justify-center rounded-md border border-grey-700 bg-white-600 py-2 px-4 text-sm font-medium shadow-lg text-grey hover:bg-slate-200 focus:outline-none focus:ring-2 focus:ring-lime-600 focus:ring-offset-2"
+                >
+                  Cancel{" "}
+                </button>
+              </div>
+              <div>
+                <button
+                  onClick={signIn}
+                  type="submit"
+                  className="group relative flex w-full justify-center rounded-md border border-transparent bg-lime-600 py-2 px-4 text-sm font-medium text-white hover:bg-lime-700 focus:outline-none focus:ring-2 focus:ring-lime-600 focus:ring-offset-2"
+                >
+                  Sign In
+                </button>
+              </div>
+            </div>
+            {/* </form> */}
             <Link href="/sellerAccount">Seller Account</Link>
           </div>
         </div>
