d0abedc11bfa211e446a201ed6a94644f4bf2032
Jenna <jennaweller7@gmail.com>
2023-03-09 12:40:45 -0800

    removingwrapofsessionprovider

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../styles/globals.css'
-import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps}) {
   return (
