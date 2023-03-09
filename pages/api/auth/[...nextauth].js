0c4b8f101ffea336e7e6324a642ec1fd4c7488c5
Jenna <jennaweller7@gmail.com>
2023-03-09 10:56:56 -0800

    addedSecret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
-//import {PrismaAdapter} from '@next-auth/prisma-adapter';
-//import {PrismaClient} from '@prisma/client';
 
 export const authOptions = {
-     //adapter: PrismaAdapter(prisma),
-    // Configure one or more authentication providers
     providers: [
       GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID,
@@ -30,64 +26,15 @@ export const authOptions = {
         }
       })
     ],
-    /*callbacks:{
-        jwt: async () => {},
-        session: () => {}
-    }, 
-    secret: "test",
-    jwt: {
-        secret: "test",
-        encryption: true,
-    },*/ 
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks:{
+      async redirect(url, baseUrl){
+        return '/sellerAccount'; 
+      },
+    },
+  
   }
   
   export default NextAuth(authOptions)
 
 
-/*
-const options = {
-    providers: [
-        GoogleProvider({
-            clientId: "process.env.GOOGLE_CLIENT_ID",
-            clientSecret: "process.env.GOOGLE_CLIENT_SECRET",
-        }),
-        Email({
-            server:{
-                host: process.env.EMAIL_SERVER_HOST,
-                port: process.env.EMAIL_SERVER_PORT,
-                auth:{
-                    user: process.env.EMAIL_SERVER_USER,
-                    pass: process.env.EMAIL_SERVER_PASSWORD,
-                },
-            },
-            form: process.env.EMAIL_FROM,
-        })
-        
-    ],
-    /* database:{
-        type:"sqlite",
-        database: ":memory:",
-        synchronize: true,
-    } 
-};
-export default (req,res) => NextAuth(req,res,options);
-/*
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    // ...add more providers here
-  ],
-
-  //Override the default sign in page
-  secret: process.env.NEXTAUTH_SECRET,
-
-  page:{
-    signIn:"/auth/signin",
-  }
-}
-
-export default NextAuth(authOptions)*/
