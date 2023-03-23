d3fa1796901267ae8eacc6ae26ea3e5307ab6618
Jenna <jennaweller7@gmail.com>
2023-03-22 19:10:45 -0700

    addedInputBox

--- pages/BecomeVendor.js
+++ pages/BecomeVendor.js
@@ -148,6 +148,18 @@ const submitHandler = (event) => {
                     </div>
                   </div>
                 </div>
+                <div className="flex flex-col w-1/2 ">
+                      <label htmlFor="bio" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="Top popover">Seller Bio</label>
+                      <input
+                        id="bio"
+                        name="bio"
+                        type="bio"
+                        autoComplete="bio"
+                        required
+                        className="relative block w-full appearance-none rounded-none rounded-t-md rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        //defaultValue={profile.produce.bio} needs to be added to database
+                      />
+                    </div>
               </div>
               <div className="flex items-center justify-between">
                 <div className="flex items-center">
