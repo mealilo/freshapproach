fd05b76929c631fdd18075b1d98c823f5fdcc855
librow <55640527+librow@users.noreply.github.com>
2023-03-29 21:14:44 -0600

    fixed single quotes

--- pages/SubscribeConfirmation.js
+++ pages/SubscribeConfirmation.js
@@ -5,7 +5,7 @@ const SubscribeConfirmation = () => {
                 <div className="flex flex-col items-center space-y-5 rounded-md shadow-xl p-5 border-t-8 border-teal-800">
                     <h2 className="text-5xl font-normal tracking-tight text-gray-900">Thanks For Subscribing!</h2>
                     <p className="text-center text-sm md:text-base">
-                        You're now subscribed to our newsletter. It's great to have you on board. You'll hear from us shortly!
+                        You&#39;re now subscribed to our newsletter. It&#39;s great to have you on board. You&#39;ll hear from us shortly!
                     </p>
                 </div>
             </div>
